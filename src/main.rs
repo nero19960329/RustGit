1ddb4355e32e0a9b01fd7e253d1d482a249e6356
nero19960329 <wangzhao2013tsinghua@gmail.com>
2024-02-27 15:07:25 +0900

    feat: implement hash-object

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand};
+use sha1::{Digest, Sha1};
 use std::env;
 use std::fs;
 
@@ -13,13 +14,28 @@ struct RustGitArgs {
 enum RustGitSubCommands {
     #[clap(name = "init")]
     Init,
+
+    #[clap(name = "hash-object")]
+    HashObject(HashObjectArgs),
+}
+
+/// Compute object ID
+#[derive(Parser, Debug)]
+struct HashObjectArgs {
+    file: String,
 }
 
 fn rgit_init() {
     let rgit_dir = env::current_dir().unwrap().join(".rgit");
-    if fs::metadata(".rgit").is_err() {
+    let rgit_dir_exist = fs::metadata(&rgit_dir).is_ok();
+    if !rgit_dir_exist {
         fs::create_dir(".rgit").unwrap();
+    }
+    if fs::metadata(".rgit/objects").is_err() {
+        fs::create_dir(".rgit/objects").unwrap();
+    }
 
+    if !rgit_dir_exist {
         println!(
             "Initialized empty RGit repository in {}",
             rgit_dir.display()
@@ -32,11 +48,24 @@ fn rgit_init() {
     }
 }
 
+fn rgit_hash_object(args: &HashObjectArgs) {
+    let file = env::current_dir().unwrap().join(&args.file);
+    let data = fs::read(&file).unwrap();
+    let mut hasher = Sha1::new();
+    hasher.update(&data);
+    let hash_result = hasher.finalize();
+    let hash = format!("{:x}", hash_result);
+    let path = format!(".rgit/objects/{}", &hash);
+    fs::write(&path, &data).unwrap();
+    println!("{}", hash);
+}
+
 fn main() {
     let args = RustGitArgs::parse();
 
     match &args.command {
         Some(RustGitSubCommands::Init) => rgit_init(),
+        Some(RustGitSubCommands::HashObject(args)) => rgit_hash_object(args),
         None => {
             println!("unknown command");
         }
