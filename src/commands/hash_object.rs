923733c8a1fc9c1e47286f16fee6726428a9ea82
nero19960329 <wangzhao2013tsinghua@gmail.com>
2024-03-12 11:59:44 +0900

    test: hash_object

--- src/commands/hash_object.rs
+++ src/commands/hash_object.rs
@@ -2,6 +2,7 @@ use super::super::objects::{Blob, RGitObject};
 use anyhow::Result;
 use clap::Parser;
 use std::env;
+use std::path;
 
 /// Compute object ID
 #[derive(Parser, Debug)]
@@ -13,13 +14,48 @@ pub struct HashObjectArgs {
     pub file: String,
 }
 
-pub fn rgit_hash_object(args: &HashObjectArgs) -> Result<()> {
-    let file = env::current_dir()?.join(&args.file);
+fn hash_object(file: &path::Path, write: bool) -> Result<()> {
     let blob = Blob::from_path(&file)?;
     let hash = blob.hash()?;
-    if args.write {
+    if write {
         blob.write_object()?;
     }
     println!("{}", hex::encode(hash));
     Ok(())
+}
+
+fn path_from_str(s: &str) -> Result<path::PathBuf> {
+    let path = path::PathBuf::from(s);
+    if path.is_relative() {
+        let current_dir = env::current_dir()?;
+        Ok(current_dir.join(path))
+    } else {
+        Ok(path)
+    }
+}
+
+pub fn rgit_hash_object(args: &HashObjectArgs) -> Result<()> {
+    let file = path_from_str(&args.file)?;
+    hash_object(&file, args.write)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env;
+    use std::fs;
+    use tempfile::tempdir;
+
+    #[test]
+    fn test_path_from_str() {
+        let dir = tempdir().unwrap();
+        env::set_current_dir(&dir).unwrap();
+        let file = dir.path().join("test.txt");
+        fs::File::create(&file).unwrap();
+        let path = path_from_str("test.txt").unwrap();
+        assert_eq!(path, file);
+
+        let path = path_from_str("/test.txt").unwrap();
+        assert_eq!(path, path::PathBuf::from("/test.txt"));
+    }
 }
